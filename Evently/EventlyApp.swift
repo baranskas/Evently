33bc5e8a8ac310f2acec3c6bd8a2a28f57d88628
DevCache1 <martynasbaranskas@gmail.com>
2024-04-16 08:28:08 +0300

    Initial Commit

---
+++ Evently/EventlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventlyApp.swift
+//  Evently
+//
+//  Created by Martynas Baranskas on 16/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct EventlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
